101426c83279339d9f8fc0305f69323219ac2f1d
kenblikylee <kenblikylee@126.com>
2020-03-08 21:29:11 +0800

    base

--- bin/vxp.js
+++ bin/vxp.js
@@ -1,4 +1,27 @@
 #!/usr/bin/env node
-const version = require("../package.json").version;
 
-console.log(`vxp-cli version ${version}`);
+const program = require("commander");
+
+program
+  .version(`vxp-cli ${require("../package").version}`)
+  .usage("<command> [options]");
+
+program
+  .command("create <app-name>")
+  .description("创建一个插件化项目。")
+  .action(name => {
+    require("../lib/create")(name);
+  });
+
+program
+  .command("add <plugin>")
+  .description("在已创建的项目中添加插件。")
+  .action(plugin => {
+    require("../lib/add")(plugin);
+  });
+
+program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
